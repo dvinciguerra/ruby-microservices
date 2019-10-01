7b58a50b40e89fab1bcd9a07c7b3ef99aa7a5890
Daniel Vinciguerra <daniel.vinciguerra@resultadosdigitais.com.br>
2019-10-01 08:42:28 -0300

    Create new services microservice

---
+++ services/app.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'http'
+require 'sinatra'
+require 'sinatra/json'
+
+AUTH_SERVICE_ENDPOINT = ENV.fetch('AUTH_SERVICE_ENDPOINT') { 'http://localhost:3001/v1/validate-token' }
+
+before do
+  request.body.rewind
+  @request_payload = JSON.parse(request.body.read, symbolize_names: true)
+
+  response = HTTP.post(
+    'http://localhost:3001/v1/validate-token', json: { token: request.env['HTTP_AUTHORIZATION'] }
+  )
+  halt(401) unless response.status.success?
+end
+
+post '/v1/services' do
+  status 201
+  json @request_payload
+end
